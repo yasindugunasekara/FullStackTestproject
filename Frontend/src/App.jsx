2ed2178c6175372bbdc94e0d5be5f894dcde20b2
yasindugunasekara <yasinduramrasu2020@gmail.com>
2026-02-19 07:18:10 +0530

    feat: add Registration page and update routing in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import StudentHome from "./pages/StudentHome";
 import AdminHome from "./pages/AdminHome";
 import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/student" element={<StudentHome />} />
         <Route path="/admin" element={<AdminHome />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ Frontend/src/pages/Registration.jsx
@@ -0,0 +1,9 @@
+import { Route } from "react-router-dom";
+
+export default function Registration() {
+    return(
+        <div className="min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-center mt-10">Registration Page</h1>
+        </div>
+    )
+}
